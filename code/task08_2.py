67b48823f8f186522a2561b501cbec7127135e1f
Ilaria Parodi <146097375+ilariaparodi@users.noreply.github.com>
2026-01-26 12:03:11 +0100

    Update task08_2.py

--- code/task08_2.py
+++ code/task08_2.py
@@ -1,21 +1,17 @@
-from typing import List
+from typing import List 
 
-def is_odd_product(a: int, b: int) -> str:
-    """
-    Determines whether the product of two integers is odd.
-    
-    Args:
-    a (int): The first integer.
-    b (int): The second integer.
-    
-    Returns:
-    str: A string indicating whether the product is odd ("Yes") or even ("No").
-    """
-    # Check if either integer is 1
-    if a == 1 or b == 1:
-        return "Yes"
-    # Check if both integers have the same parity (both odd or both even)
-    elif a % 2 == b % 2:
+
+def is_odd_product(a: int, b: int) -> str: 
+    """Statement 
+You are given integers A and B, each between 1 and 3 (inclusive). 
+
+Determine if there is an integer C between 1 and 3 (inclusive) such that A \times B \times C is an odd number.""" 
+
+    # The product of three integers is odd if and only if all three integers are odd. 
+    # We are given that A and B are between 1 and 3. 
+    # If both a and b are odd, then we can choose an odd c (1 or 3) 
+    # to make the product a * b * c odd. 
+    if a % 2 != 0 and b % 2 != 0: 
+        return "Yes" 
+    else: # If either a or b is even, then the product a * b will be even. 
         return "No"
-    else:
-        return "Yes"
